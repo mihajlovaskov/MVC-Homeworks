110857d4eb6829beaa1a4749535c96c27c670351
mihajlovaskov <mihajlovaskov@gmail.com>
2022-06-15 23:54:28 +0200

    Class02Homework

---
+++ HomeworkClass02/SEDC.PizzaApp/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace SEDC.PizzaApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Size { get; set; }
+    }
+}
