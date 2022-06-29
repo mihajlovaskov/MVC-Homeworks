b6db27ca51615e93dc40c56b03208f905a72a2c7
mihajlovaskov <mihajlovaskov@gmail.com>
2022-06-29 23:54:45 +0200

    HomeworkClass04

---
+++ HomeworkClass04/SEDC.PizzaApp/Models/Mappers/UsersMapper.cs
@@ -0,0 +1,19 @@
+﻿using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+
+namespace SEDC.PizzaApp.Models.Mappers
+{
+    public static class UsersMapper
+    {
+        public static UsersViewModel ToUsersViewModel(User usersDb)
+        {
+            return new UsersViewModel
+            {
+                Id = usersDb.Id,
+                FirstName = usersDb.FirstName,
+                LastName = usersDb.LastName,
+                PhoneNumber = usersDb.PhoneNumber,
+            };
+        }
+    }
+}
---
+++ HomeworkClass04/SEDC.PizzaApp/Models/ViewModels/UsersViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SEDC.PizzaApp.Models.ViewModels
+{
+    public class UsersViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
